20d4e89340f6121c89482e326009e5e5ff19be6f
Kai <kaizheng31@gmail.com>
2021-02-04 16:40:36 -0500

    Finished fondling.

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from putDiary import diaryOrg
 from getDiary import getDiary
 from evaluate import wordCountScore
+import os
 app = Flask(__name__)
-
+app.secret_key = os.urandom(24)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     diary = None
 
     if request.method == "POST":
-        userInput = request.form['abc']
+        userInput = request.form['diary']
         if diaryOrg(userInput):
-            diary = getDiary(wordCountScore(userInput))
-            print(diary)
+            session['diary'] = getDiary(wordCountScore(userInput))
+            return redirect(url_for('diary'))
 
 
     return render_template('index.html')
 
+@app.route("/diary", methods=["POST", "GET"])
+def diary():
+    diary = session.get('diary')
+
+
+    return render_template('diary.html', diary = diary)
 
 app.run(debug=True)
