94a007dcbd9ad9abc55de4556ea9aed396f35aed
Kai <kaizheng31@gmail.com>
2021-02-08 12:09:56 -0500

    fix

--- getDiary.py
+++ getDiary.py
@@ -3,7 +3,7 @@ import random, os
 # gets a random diary of quality quality
 # use this in the flask thing
 def getDiary(quality):
-    rand = random.randint(0, len(os.listdir("diaries/" + str(quality))) - 2)
+    rand = random.randint(1, len(os.listdir("diaries/" + str(quality))) - 2)
     diary = open("diaries/" + str(quality) + "/" + str(rand))
     content = diary.read()
     diary.close()
